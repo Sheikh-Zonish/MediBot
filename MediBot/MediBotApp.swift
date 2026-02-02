acd1085a0a2d811ba7269a5fb020a04a702a8c3f
Zonish Sheikh <zonishsheikh@outlook.com>
2026-02-02 11:20:06 +0000

    Initial Commit

---
+++ MediBot/MediBotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediBotApp.swift
+//  MediBot
+//
+//  Created by Zonish Sheikh on 02/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MediBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
